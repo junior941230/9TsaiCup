d8aa16d2f7971e01a5f86aa104468a1afd1549ef
Pangolin <junior941230@gmail.com>
2026-03-06 21:20:17 +0800

    v1.1

--- FinMindApi.py
+++ FinMindApi.py
@@ -21,8 +21,28 @@ class FinMindApi:
         
         return price0050Start, priceTAIEXStart
     
+    def getAdjustedDate(self):
+        currentTime = datetime.datetime.now()
+        adjustedDate = currentTime.date()
+        
+        # 設定時間基準點為 1:30 (若你的 1.30 指的是下午 1:30，請將參數改為 13, 30)
+        targetTime = datetime.time(1, 30)
+        
+        # 條件一：如果還沒到當天的 1:30，日期減去一天變成昨天
+        if currentTime.time() < targetTime:
+            adjustedDate -= datetime.timedelta(days=1)
+            
+        # 條件二：如果是假日，則退回上一個工作日 (星期五)
+        # weekday() 回傳值：0 是星期一 ... 5 是星期六，6 是星期日
+        if adjustedDate.weekday() == 5:
+            adjustedDate -= datetime.timedelta(days=1)
+        elif adjustedDate.weekday() == 6:
+            adjustedDate -= datetime.timedelta(days=2)
+            
+        return adjustedDate.strftime("%Y-%m-%d")
+
     def getNowPrice(self):
-        today = datetime.date.today().strftime("%Y-%m-%d")
+        today = self.getAdjustedDate()
         df = self.api.taiwan_stock_daily(
             stock_id='0050',
             start_date=today,
@@ -36,7 +56,7 @@ class FinMindApi:
         return price0050Now, priceTAIEXNow
     
     def getHistoryData(self, stock_id):
-        today = datetime.date.today().strftime("%Y-%m-%d")
+        today = self.getAdjustedDate()
         df = self.api.taiwan_stock_daily(
             stock_id=stock_id,
             start_date='2026-03-03',
